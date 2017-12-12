0975f431157372e6274b4f19efca6481f0285cab
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 20:51:44 +0100

    Exercice

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 2 : Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      $maVariable = 'Homme';
+
+      if ($maVariable = 'Homme') {
+        echo 'C\'est une développeuse !!!';
+      }
+      else {
+         'C\'est un développeur !!!';
+      }
+    ?>
+  </body>
+</html>
